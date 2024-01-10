c6deec26aa7c866ec90739f257928bb953558c9b
Santeri <122432859+Peasami@users.noreply.github.com>
2024-01-10 11:28:48 +0200

    done

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
@@ -9,17 +9,19 @@ export default function App() {
 
 
   function calculateHeartRate() {
-    const lowerLimit = (220-age) * 0.65;
-    const upperLimit = (220-age) * 0.85;
-    setHeartRateLimits(lowerLimit + ' - ' + upperLimit);
+    const lowerLimit = (220-age.replace(',','.')) * 0.65;
+    const upperLimit = (220-age.replace(',','.')) * 0.85;
+    setHeartRateLimits(lowerLimit.toFixed(2) + ' - ' + upperLimit.toFixed(2));
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.text1}>Age</Text>
-      <TextInput style={styles.text1} value={age} onChangeText={calculateHeartRate}/>
+      <TextInput style={styles.text1} value={age} onChangeText={text => setAge(text)}
+        keyboardType='decimal-pad'/>
       <Text style={styles.text1}>Heart rate limits</Text>
       <Text style={styles.text1}>{heartRateLimits}</Text>
+      <Button title="Calculate" onPress={calculateHeartRate}></Button>
       <StatusBar style="auto" />
     </View>
   );
