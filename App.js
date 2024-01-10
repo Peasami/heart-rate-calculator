70535ff9338de3bd4e5145ccc0668def21e29bde
Santeri <santeri.hyry@gmail.com>
2024-01-10 11:08:56 +0200

    update

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [age, setAge] = useState('');
+  const [heartRateLimits, setHeartRateLimits] = useState('');
+
+
+  function calculateHeartRate() {
+    const lowerLimit = (220-age) * 0.65;
+    const upperLimit = (220-age) * 0.85;
+    setHeartRateLimits(lowerLimit + ' - ' + upperLimit);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>Age</Text>
+      <TextInput style={styles.text1} value={age} onChangeText={calculateHeartRate}/>
+      <Text style={styles.text1}>Heart rate limits</Text>
+      <Text style={styles.text1}>{heartRateLimits}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#012',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text1: {
+    color: '#fff',
+    fontSize: 30,
   },
 });
